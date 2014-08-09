ff8f993d2c8b48200968b2a60da13619981c58ea
kimiyuki shirai <kimiyuki@gmail.com>
2014-08-09 16:59:49 +0900

    first commit

---
+++ GA Data Exporter/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GA_Data_Exporter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(183, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(504, 266);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 21;
+            this.dataGridView2.Size = new System.Drawing.Size(509, 409);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 266);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(454, 399);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 677);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ GA Data Exporter/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Google.Apis;
+using Google.Apis.Util.Store;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Analytics.v3;
+using Google.Apis.Services;
+using Google.Apis.Requests;
+using Google.Apis.Analytics.v3.Data;
+
+
+namespace GA_Data_Exporter
+{
+    public partial class Form1 : Form
+    {
+        private AnalyticsService service;
+        private UserCredential credential;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                auth();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var ee in ex.InnerExceptions)
+                {
+                    MessageBox.Show(ee.Message);
+                    //Console.WriteLine("ERROR: " + ee.Message);
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getMetaData();
+        }
+
+        private void auth()
+        {
+            
+            credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                           new ClientSecrets
+                           {
+                               ClientId = "643412917207.apps.googleusercontent.com",
+                               ClientSecret = "NoEhLrjYIN10wIn41vUdCSB-"
+                           },
+                           new[] { AnalyticsService.Scope.AnalyticsReadonly },
+                           "user", CancellationToken.None, new FileDataStore("ga")).Result;
+             service = new AnalyticsService(
+                new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "abc-analytics ga data exporter"
+                });
+        }
+
+        private void getMetaData()
+        {
+            MetadataResource.ColumnsResource.ListRequest req = service.Metadata.Columns.List("ga");
+            Columns metadataList = req.Execute();
+            IList<Column> items = metadataList.Items;
+            var sitems = items.Where(c => c.Attributes["status"] != "DEPRECATED").ToList();
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("id", typeof(string));
+            dt.Columns.Add("group", typeof(string));
+            dt.Columns.Add("ui name", typeof(string));
+            dt.Columns.Add("description", typeof(string));
+            var metrics = sitems.Where(c => c.Attributes["type"] == "METRIC").ToList();
+            for (int k = 0; k < metrics.Count; k++)
+            {
+                DataRow row = dt.NewRow();
+                var metric = metrics[k];
+                row[0] = metrics[k].Id;
+                row[1] = metrics[k].Attributes["group"];
+                row[2] = metrics[k].Attributes["uiName"];
+                row[3] = metrics[k].Attributes["description"];
+                dt.Rows.Add(row);
+            }
+            this.dataGridView1.DataSource = dt;
+
+            DataTable dt1 = new DataTable();
+            dt1.Columns.Add("id", typeof(string));
+            dt1.Columns.Add("group", typeof(string));
+            dt1.Columns.Add("ui name", typeof(string));
+            dt1.Columns.Add("description", typeof(string));
+            var dimensions = sitems.Where(c => c.Attributes["type"] == "DIMENSION").ToList();
+            for (int k = 0; k < dimensions.Count; k++)
+            {
+                DataRow row = dt1.NewRow();
+                row[0] = dimensions[k].Id;
+                row[1] = dimensions[k].Attributes["group"];
+                row[2] = dimensions[k].Attributes["uiName"];
+                row[3] = dimensions[k].Attributes["description"];
+                dt1.Rows.Add(row);
+            }
+            this.dataGridView2.DataSource = dt1;
+
+
+        }
+
+    }
+}
